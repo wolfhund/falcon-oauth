06759ad15799b4b2553cfbcfe18a69391629e942
Carlos (´･ω･`) <carlos@decidata.tv>
2017-05-25 18:44:00 -0500

    renames class

--- tests/app.py
+++ tests/app.py
@@ -1,11 +1,11 @@
 import falcon
 
-from falcon_oauth.oauth2.views.authorization import Authenticate
+from falcon_oauth.oauth2.views.authorization import AuthorizationCodeGrant
 
 
 application = api = falcon.API()
 
-authenticate = Authenticate()
-AUTH_PAGE_URI = '/oauth2/auth'
-api.add_route(AUTH_PAGE_URI, authenticate)
+authorization_code_grant = AuthorizationCodeGrant()
+AUTHORIZATION_CODE_GRANT_PAGE_URI = '/oauth2/grant/authorizationcode/auth'
+api.add_route(AUTHORIZATION_CODE_GRANT, authorization_code_grant)
 
