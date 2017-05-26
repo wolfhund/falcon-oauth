da79bfdfd6bf6bd4cede7ed0281b7c074debed32
Carlos (´･ω･`) <carlos@decidata.tv>
2017-05-26 17:16:52 -0500

    renames AuthorizationCodeView to Authorization

--- tests/app.py
+++ tests/app.py
@@ -5,6 +5,6 @@ from falcon_oauth.oauth2.views.authorization import Authorization
 
 application = api = falcon.API()
 
-authorization_view = AuthorizationCodeView()
+authorization_view = Authorization()
 AUTHORIZATION_URI = '/oauth2/authorize/'
 api.add_route(AUTHORIZATION_URI, authorization_view)
