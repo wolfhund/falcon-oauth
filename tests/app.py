cd329d9ccd0d5b3fd6147f5238645dd89831871d
Carlos (´･ω･`) <carlos@decidata.tv>
2017-05-26 13:39:25 -0500

    remove code and renames files

--- tests/app.py
+++ tests/app.py
@@ -1,11 +1,10 @@
 import falcon
 
-from falcon_oauth.oauth2.views.authorization_code_view import AuthorizationCodeView
+from falcon_oauth.oauth2.views.authorization import Authorization
 
 
 application = api = falcon.API()
 
-authorization_code_view = AuthorizationCodeView()
-AUTHORIZATION_CODE_VIEW_PAGE_URI = '/oauth2/authorizationcode/auth'
-api.add_route(AUTHORIZATION_CODE_GRANT, authorization_code_view)
-
+authorization_view = AuthorizationCodeView()
+AUTHORIZATION_URI = '/oauth2/authorize/'
+api.add_route(AUTHORIZATION_URI, authorization_view)
