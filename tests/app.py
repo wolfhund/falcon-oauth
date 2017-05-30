9ba46f14e9103bf29ecc00b524556eaca3bba8fa
Carlos (´･ω･`) <carlos@decidata.tv>
2017-05-30 18:28:22 -0500

    add ping test

--- tests/app.py
+++ tests/app.py
@@ -2,7 +2,7 @@ import falcon
 
 from falcon_oauth.oauth2.views.authorization import Authorization
 from falcon_oauth.oauth2.views.token import Token
-
+from falcon_oauth.oauth2.views.ping import Ping
 
 application = api = falcon.API()
 
@@ -13,3 +13,7 @@ api.add_route(AUTHORIZATION_URI, authorization_view)
 token_view = Token()
 TOKEN_URI = '/oauth2/token/'
 api.add_route(TOKEN_URI, token_view)
+
+ping = Ping()
+PING_URI = '/ping'
+api.add_route(PING_URI, ping)
