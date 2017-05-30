78aba36301274a16d60dd897007cbd2cd06193d0
Carlos (´･ω･`) <carlos@decidata.tv>
2017-05-30 18:28:42 -0500

    small fix in wrapper

--- falcon_oauth/oauth2/decorators/oauth2_provider.py
+++ falcon_oauth/oauth2/decorators/oauth2_provider.py
@@ -1,3 +1,4 @@
+import functools
 import falcon
 from oauthlib.oauth2 import BackendApplicationServer
 from falcon_oauth.oauth2.validators.oauth2_request_validator import OAuth2RequestValidator
@@ -11,7 +12,7 @@ class OAuth2ProviderDecorator(object):
     def protected_resource_view(self, scopes=None):
         def decorator(f):
             @functools.wraps(f)
-            def wrapper(req):
+            def wrapper(self_obj, req,res, *args, **kwargs):
                 # Get the list of scopes
                 try:
                     scopes_list = scopes(req)
@@ -20,24 +21,24 @@ class OAuth2ProviderDecorator(object):
 
                 valid, r = self._resource_endpoint.verify_request(
                         req.uri, 
-                        http_method=req.method, 
-                        body=req.stream.read(), 
-                        headers=req.headers, 
-                        scopes_list=scopes_list
+                        req.method, 
+                        req.stream.read(), 
+                        req.headers, 
+                        scopes_list
                 )
 
                 # For convenient parameter access in the view
-                add_params(req, {
-                    'client': r.client,
-                    'user': r.user,
-                    'scopes': r.scopes
-                })
+                #add_params(req, {
+                #    'client': r.client,
+                #    'user': r.user,
+                #    'scopes': r.scopes
+                #})
                 if valid:
-                    return f(req)
+                    return f(self_obj, req, res)
                 else:
                     # Framework specific HTTP 403
-                    req.body = '{"error": "forbidden"}'
-                    req.status_code = falcon.HTTP_403
+                    res.body = '{"error": "forbidden"}'
+                    res.status = falcon.HTTP_403
             return wrapper
         return decorator
 
